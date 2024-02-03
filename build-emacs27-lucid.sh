e18b2983211bbd98e098d9235b2160015d31913f
junghan0611 <31724164+junghan0611@users.noreply.github.com>
2024-02-04 07:47:51 +0900

    clean up

--- build-emacs27-lucid.sh
+++
@@ -1,8 +0,0 @@
-rm -r ~/emacs27-lucid
-cd ../emacs27-lucid
-git clean -fdx
-git pull origin emacs-27
-./autogen.sh
-./configure --prefix=$HOME/emacs27lucid --with-x-toolkit=lucid --with-modules --with-modules --with-rsvg --with-json
-make -j4
-make install
--- build-emacs27.sh
+++
@@ -1,8 +0,0 @@
-rm -r ~/emacs27 
-cd ../emacs27
-git clean -fdx
-git pull origin emacs-27
-./autogen.sh
-./configure --with-modules --with-json --with-rsvg --prefix=$HOME/emacs27 --bindir=$HOME/bin/
-make -j4
-make install
